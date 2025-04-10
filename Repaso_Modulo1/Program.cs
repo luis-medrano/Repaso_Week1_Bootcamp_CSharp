83b054ee4f1ecc1152596ac2cce16e4439991038
Luis Medrano <you@example.com>
2025-04-09 21:57:12 -0500

    Reto 2 - Clase 09 abril 2025

--- Repaso_Modulo1/Program.cs
+++ Repaso_Modulo1/Program.cs
@@ -14,7 +14,7 @@
              */
 
             // For loop:
-            int i, j, filas;
+            int i, j, k, filas;
             Console.WriteLine("Ingresa el numero de filas");
             filas = Convert.ToInt32(Console.ReadLine());
 
@@ -22,7 +22,7 @@
             {
                 for (j = 1; j <= i; j++)
                 {
-                    Console.Write(j);
+                    Console.Write("*");
                 }
                 Console.Write("\n");
             }
@@ -34,6 +34,37 @@
              * 1 2 3
              * 1 2 3 4
              */
+            for (i = 1; i <= filas; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Se acabo el bucle");
+
+            /*
+             * -----*
+             * ----* *
+             * ---* * *
+             * --* * * *
+             * -* * * * *
+             */
+            for (i = 1; i <= filas; i++)
+            {
+                for (j = 1; j <= filas-i; j++)
+                {
+                    Console.Write("-");
+                }
+
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Se acabo el bucle");
         }
     }
 }
