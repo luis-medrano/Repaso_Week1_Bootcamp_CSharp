492ccff3828de5a4a545fce66f3593f12ef63580
Luis Medrano <you@example.com>
2025-04-09 21:40:29 -0500

    Reto Bucle - Clase 09 Abril 2025

---
+++ Repaso_Modulo1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Repaso_Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==================");
+            Console.WriteLine("Repaso bucles");
+            /*
+             * *
+             * * *
+             * * * *
+             * * * * *
+             */
+
+            // For loop:
+            int i, j, filas;
+            Console.WriteLine("Ingresa el numero de filas");
+            filas = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= filas; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Se acabo el bucle");
+
+            /*
+             * 1
+             * 1 2
+             * 1 2 3
+             * 1 2 3 4
+             */
+        }
+    }
+}
