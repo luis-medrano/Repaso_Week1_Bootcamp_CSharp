6ef95065875b30579f5b8c80be31919fc7f59c88
Luis Medrano <you@example.com>
2025-04-09 23:09:33 -0500

    Reto duplicados

--- Repaso_Modulo1/Program.cs
+++ Repaso_Modulo1/Program.cs
@@ -83,7 +83,7 @@
             {
                 for (k = espacio; k >= 1; k--)
                 {
-                    Console.Write("-");
+                    Console.Write(" ");
                 }
 
                 for (j = 1; j <= i; j++)
@@ -91,9 +91,64 @@
                     Console.Write("*");
                 }
                 Console.Write("\n");
-                espacio--;
+                espacio--; // Restando los espacios para que tome forma de piramide
             }
             Console.WriteLine("Se acabo el bucle");
+
+
+            // Ejercicios de arreglos
+            // Listar
+
+            int[] otrosNumeros = new int[] { 10, 20, 30, 40, 50 };
+
+            for (i = 0; i<otrosNumeros.Length; i++)
+            {
+                Console.WriteLine("El valor del array es " + otrosNumeros[i]);
+            }
+
+            int total2 = 0;
+            for (int i1 = 0; i1 < otrosNumeros.Length; i1++)
+            {
+                total2 += otrosNumeros[i1];
+            }
+
+            //Obtener
+            int total = 0;
+            foreach (int valor in otrosNumeros)
+                //total=total+valor;
+                total += valor;
+            Console.WriteLine(total.ToString());
+
+
+            // Ejercicio
+            // Contar los duplicados dentro de un arreglo
+            // int[] arreglo = new int[] {44, 44, 32, 35}
+            // Resultado: Hay 2 valores duplicados en el arreglo
+
+            int[] numerosArray = new int[] { 44, 44, 32, 35, 50 };
+            int[] numerosTemporal = new int[numerosArray.Length];
+            total = 0;
+
+            for (int i1 = 0; i1 < numerosArray.Length; i1++)
+            {
+                numerosTemporal[i1] = numerosArray[i1];
+            }
+
+            for (int i1 = 0; i1 < numerosArray.Length; i1++)
+            {
+                for (int j1 = 0; j1 < numerosArray.Length; j1++)
+                {
+                    if (numerosArray[i1] == numerosTemporal[j1])
+                    {
+                        total++;
+                    }
+                }
+                if (total > 0) 
+                    Console.WriteLine("El numero " + numerosArray[i1] + " tiene repetidos: " + total);
+                else
+                    Console.WriteLine("El numero " + numerosArray[i1] + " no tiene repetidos");
+                total = 0;
+            }
         }
     }
 }
